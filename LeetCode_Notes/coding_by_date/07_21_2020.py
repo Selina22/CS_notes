2a836662b716c24db1078917b1e9e1eabd526c9e
Selina Wan <52523067+Selina22@users.noreply.github.com>
2020-07-21 22:04:43 -0400

    Update 07_21_2020.py

--- LeetCode_Notes/coding_by_date/07_21_2020.py
+++ LeetCode_Notes/coding_by_date/07_21_2020.py
@@ -36,3 +36,30 @@ class Solution:
             prev1 = curr
         return curr
             
+
+'''
+213. House Robber II
+You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed. 
+All houses at this place are arranged in a circle. That means the first house is the neighbor of the last one. 
+Meanwhile, adjacent houses have security system connected and it will automatically contact the police if two adjacent houses 
+were broken into on the same night.
+
+Given a list of non-negative integers representing the amount of money of each house, 
+determine the maximum amount of money you can rob tonight without alerting the police.
+'''
+class Solution:
+    def rob_by_pos(self, nums: List[int], start: int, end: int):
+        prev1 = 0
+        prev2 = 0
+        curr = 0
+        for i in range(start, end):
+            curr = max(prev1, nums[i]+prev2)
+            prev2 = prev1
+            prev1 = curr
+        return curr
+    def rob(self, nums: List[int]) -> int:
+        total = len(nums)
+        if (total == 1):
+            return nums[0]
+        return max(Solution().rob_by_pos(nums, 0, total-1), Solution().rob_by_pos(nums, 1, total))
+        
