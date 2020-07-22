f05e27bc688dc00061d95ff760d11a23b5d1d010
Selina Wan <52523067+Selina22@users.noreply.github.com>
2020-07-21 21:03:39 -0400

    Update 07_19_2020.py

--- LeetCode_Notes/coding_by_date/07_19_2020.py
+++ LeetCode_Notes/coding_by_date/07_19_2020.py
@@ -58,3 +58,13 @@ class Solution:
         return False
 
 
+‘’‘
+215. Kth Largest Element in an Array
+Find the kth largest element in an unsorted array. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+’‘’
+#can try quick sort algo for this later
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        return nums[len(nums)-k]
+   
